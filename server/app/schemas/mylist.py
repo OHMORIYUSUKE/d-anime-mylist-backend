50e17e90cb79aae4ec62fc93afaef32384e36088
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-03-24 03:42:31 +0900

    スキーマに説明を追加

--- server/app/schemas/mylist.py
+++ server/app/schemas/mylist.py
@@ -1,28 +1,28 @@
 from datetime import datetime, time, timedelta
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List
 
 
 class MyListPost(BaseModel):
-    url: str
+    url: str = Field(..., description="dアニメストアのマイリストのURL")
 
 
 class MyListContent(BaseModel):
-    title: str
-    image: str
-    url: str
+    title: str = Field(..., description="アニメのタイトル")
+    image: str = Field(..., description="アニメのサムネイル画像のパス")
+    url: str = Field(..., description="アニメの詳細ページ（d-anime ストアのページ）")
 
 
 class MyListGet(BaseModel):
-    id: str
-    d_anime_store_url: str
-    created_at: datetime
-    updated_at: datetime
-    mylist: List[MyListContent]
+    id: str = Field(..., description="マイリストのID")
+    d_anime_store_url: str = Field(..., description="dアニメストアのマイリストのURL")
+    created_at: datetime = Field(..., description="データが作成された時間")
+    updated_at: datetime = Field(..., description="データが更新された時間")
+    mylist: List[MyListContent] = Field(..., description="マイリストに登録されているアニメのリスト", max_item=500)
 
 
 class MyListInfo(BaseModel):
-    id: str
-    d_anime_store_url: str
-    created_at: datetime
-    updated_at: datetime
+    id: str = Field(..., description="マイリストのID")
+    d_anime_store_url: str = Field(..., description="dアニメストアのマイページのURL")
+    created_at: datetime = Field(..., description="データが作成された時間")
+    updated_at: datetime = Field(..., description="データが更新された時間")
