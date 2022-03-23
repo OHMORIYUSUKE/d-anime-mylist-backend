6457ff323e008db47c5f080d2e875e97eece6b72
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-03-23 17:21:34 +0900

    環境変数を設定

--- server/app/db.py
+++ server/app/db.py
@@ -2,11 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+import os
+
 # 接続したいDBの基本情報を設定
-DB_USER_NAME = "test"
-DB_PW = "test"
-DB_HOST = "db"
-DB_NAME = "danime"
+# DB_USER_NAME = "test"
+# DB_PW = "test"
+# DB_HOST = "db"
+# DB_NAME = "danime"
+DB_USER_NAME = os.environ.get('DB_USER_NAME')
+DB_PW = os.environ.get('DB_PW')
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
 
 DATABASE = "mysql://%s:%s@%s/%s?charset=utf8" % (
     DB_USER_NAME,
