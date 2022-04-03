be057c3c16a3f3f8bd2c80613a6d6b87b2bda685
Aruminium <b2201680@photon.chitose.ac.jp>
2022-04-04 00:08:31 +0900

    #2 add function

--- server/app/api/service/scrape.py
+++ server/app/api/service/scrape.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from bs4 import ResultSet
+import time
 
 from fastapi import FastAPI, HTTPException
 
@@ -32,12 +33,31 @@ class Scrape:
         title_elm_list: List[ResultSet] = soup.find_all("span", class_="ui-clamp")
         image_elm_list: List[ResultSet] = soup.find_all("img")
         link_elm_list: List[ResultSet] = soup.find_all("a", class_="itemModuleIn")
+        print(title_elm_list)
+        print(image_elm_list)
+        print(link_elm_list)
+        stories_elm_list = []
+        for link_elm in link_elm_list:
+            print("リンク："+link_elm.get("href"))
+            next_soup = self.__get_html(link_elm.get("href"))
+            first_title_elm_list = next_soup.find("span", class_="ui-clamp webkit2LineClamp")
+            print(first_title_elm_list)
+
+            stories_tmp = next_soup.find("div", class_="titleWrap")
+            stories = " "
+            try:
+                stories = stories_tmp.h1.span.text
+                print(stories_tmp.h1.span.text)
+            except:
+                pass
+            stories_elm_list.append(stories)
+
 
         mylist_list: List[mylist_schema.MyListContent] = []
-        for (title_elm, link_elm, image_elm) in zip(title_elm_list, link_elm_list, image_elm_list):
+        for (title_elm, link_elm, image_elm, first_title_elm, stories_elm) in zip(title_elm_list, link_elm_list, image_elm_list, first_title_elm_list, stories_elm_list):
             mylist_list.append(
                 mylist_schema.MyListContent(
-                    title=title_elm.text, image=image_elm.get("data-src"), url=link_elm.get("href")
+                    title=title_elm.text, image=image_elm.get("data-src"), url=link_elm.get("href"), first=first_title_elm.text, stories=stories_elm
                 )
             )
 
