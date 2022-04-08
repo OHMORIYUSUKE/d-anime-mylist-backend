69818cc2f533125f64267ff0f8bc17b89801643f
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-04-08 23:30:31 +0900

    testを修正

--- server/app/test/test_post_err.py
+++ server/app/test/test_post_err.py
@@ -1,7 +1,7 @@
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 
-from .config import client, DANIME_MYLISTPAGE_BASE_URL, invalid_id_page, invalid_id_db, invalid_url
+from .config import client, DANIME_MYLISTPAGE_BASE_URL, invalid_id_page, invalid_id_db, invalid_url, id
 
 """
 例外テスト
@@ -21,3 +21,9 @@ def test_post_invalid_url_2():
     response = client.post("/my-list", json={"url": invalid_url})
     assert response.json()["detail"] == "this url is not d-anime mylist page."
     assert response.status_code == 402
+
+
+# 存在しているid
+def test_post_invalid_url_3():
+    response = client.post("/my-list", json={"url": f"{DANIME_MYLISTPAGE_BASE_URL}?shareListId={id}"})
+    assert response.status_code == 409
